fe7da8771fbce836245cf079012d79afd7fc909d
VCap1 <firehack.v56@gmail.com>
2021-07-02 12:44:04 +0200

    Update counting_duplicates_6kyu.py

--- counting_duplicates_6kyu.py
+++ counting_duplicates_6kyu.py
@@ -3,3 +3,10 @@ def duplicate_count(text):
     c = Counter(text.lower())
     counter = len([x for x in c if c[x] > 1])
     return counter
+
+
+'''
+not my solution, but my personal favorite by codewars user WOnder93
+
+return sum(1 for c, n in Counter(text.lower()).iteritems() if n > 1)
+'''
