af2a7b140c495f096771e223de94c9de6b862fd3
VCap1 <firehack.v56@gmail.com>
2021-07-13 12:44:42 +0200

    Update simple_pig_latin_5kyu.py

--- simple_pig_latin_5kyu.py
+++ simple_pig_latin_5kyu.py
@@ -1,3 +1,2 @@
-import string 
 def pig_it(text):
-    return " ".join( [t[1:] + t[:1] + 'ay' if t not in list(string.punctuation) else t for t in text.split(" ")  ])
+    return " ".join( [t[1:] + t[:1] + 'ay' if t.isalpha() else t for t in text.split(" ")  ])
